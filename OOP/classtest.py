19f4578d255ffed2378707de2f1aa76a56e660e4
Brikarl <627639128@qq.com>
2019-09-23 14:52:33 +0800

    exercise2

--- OOP/classtest.py
+++ OOP/classtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-class UserData:
+class UserData(object):
     def __init__(self, iden, name):
         self.name = name
         self.iden = iden
@@ -8,9 +8,16 @@ class UserData:
     def __repr__(self):
         return "ID:{} Name:{}".format(self.name, self.iden)
 
+class NewUser(UserData):
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, value):
+        self.name = value
 
 if __name__ == '__main__':
-    user1 = UserData(101, 'Jack')
-    user2 = UserData(102, 'Louplus')
+    user1 = NewUser(101, 'Jack')
+    user1.set_name('Jackie')
+    user2 = NewUser(102, 'Louplus')
     print(user1)
     print(user2)
