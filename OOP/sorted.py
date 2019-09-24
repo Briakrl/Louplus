642a30246d05ed5bf420de66875a933fe2e6a98f
Brikarl <627639128@qq.com>
2019-09-24 18:00:33 +0800

    advanced

---
+++ OOP/sorted.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+pp = [('Leborn James', 98), ('Kevin Durant', 97), ('James Harden', 96), ('Stephen Curry', 95), ('Anthony Davis', 94)]
+
+result_list = sorted(pp)
+print(result_list)
